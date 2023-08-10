3cd42dc288dbd30b1be836c4d44c7f27530bec69
datdragon777 <ducdat612001@gmail.com>
2023-08-10 17:48:50 +0700

    Make example about applying conditional rendering

--- react-example/src/main.jsx
+++ react-example/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './passing-props-to-component/App.jsx'
+// import App from './passing-props-to-component/App.jsx'
+// import UnnecessaryState from './remove-unnecessary-state/UnnecessaryState.jsx'
+// import Gallery from './complete-the-gallery/Gallery.jsx'
+// import Form from './fix-stuck-form-input/Form'
+// import LightSwitch from './responding-to-events/LightSwitch'
+import DrinkList from './coditional-rendering/DrinkList'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <UnnecessaryState /> */}
+    {/* <Gallery /> */}
+    {/* <Form /> */}
+    {/* <LightSwitch /> */}
+    <DrinkList />
   </React.StrictMode>,
 )
