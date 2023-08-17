0ed344965f77517fc6a2fae1268c4dd434cf89cf
datdragon777 <ducdat612001@gmail.com>
2023-08-17 17:34:09 +0700

    Make example about creating picture storybook

---
+++ react-storybook-example/src/stories/Picture/Picture.stories.jsx
@@ -0,0 +1,36 @@
+import { Picture } from "./Picture";
+import styles from "./Picture.module.css"
+import { getTemplate } from "../../../helpers/storybook";
+
+const Template = getTemplate(Picture, styles)
+
+export default {
+  title: "Picture",
+  component: Picture,
+  tags: ["autodocs"],
+  args: {
+    src: "https://picsum.photos/id/1033/400/400",
+    width: 200,
+  },
+  argTypes: {
+    width: {control: "number"},
+    height: {control: "number"}
+  },
+};
+
+export const Default = Template.bind({})
+
+export const Rounded = Template.bind({})
+Rounded.args = {
+    isRounded: true,
+}
+
+export const Height = Template.bind({})
+Height.args = {
+    height: 400,
+}
+
+export const Width = Template.bind({})
+Width.args = {
+    width: 400,
+}
