39eceb2d5d856d1ca9514c953932188d79442117
datdragon777 <ducdat612001@gmail.com>
2023-08-29 08:40:11 +0700

    Update router in App.jsx

--- practice-one/src/App.jsx
+++ practice-one/src/App.jsx
@@ -1,15 +1,23 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  BrowserRouter as Router,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Analytics from "./pages/Analytics/Analytics"
+import Analytics from "./pages/Analytics/Analytics";
+import LeftSidebar from "./layouts/LeftSidebar/LeftSidebar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
+      <aside>
+        <LeftSidebar />
+      </aside>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/analytics" element={<Analytics />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
