8819b0d168a2504ee2cff39a60deb021eaaeaed4
datdragon777 <ducdat612001@gmail.com>
2023-08-29 09:11:08 +0700

    Create account component and storybook

---
+++ practice-one/src/components/Account/Account.stories.jsx
@@ -0,0 +1,14 @@
+import Account from "./Account";
+
+export default {
+    title: "Components/Account",
+    component: Account,
+    tags: ["autodocs"]
+}
+
+const Template = (args) => <Account {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+    
+}
