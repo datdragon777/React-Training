d71ff7489f5fc356bd610b5a0f5285120358b1e4
datdragon777 <ducdat612001@gmail.com>
2023-09-06 08:56:36 +0700

    Update arguments for Account storybook

--- practice-one/src/components/Account/Account.stories.jsx
+++ practice-one/src/components/Account/Account.stories.jsx
@@ -1,12 +1,18 @@
 import Account from "./Account";
+import { accountAvatar, logoutIcon } from "@assets/images";
 
 export default {
-    title: "Components/Account",
-    component: Account,
-    tags: ["autodocs"]
-}
+  title: "Components/Account",
+  component: Account,
+  tags: ["autodocs"],
+};
 
-const Template = (args) => <Account {...args} />
+const Template = (args) => <Account {...args} />;
 
-export const Default = Template.bind({})
-Default.args = {}
+export const Default = Template.bind({});
+Default.args = {
+  username: "Easin Arafat",
+  avatar: accountAvatar,
+  desc: "Free Account",
+  icon: logoutIcon,
+};
