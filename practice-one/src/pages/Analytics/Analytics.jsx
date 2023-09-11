06637a829d523b7149e7a3a8874ff61a2b4b2102
datdragon777 <ducdat612001@gmail.com>
2023-09-11 09:33:52 +0700

    Update split function and style for routers and analytics

--- practice-one/src/pages/Analytics/Analytics.jsx
+++ practice-one/src/pages/Analytics/Analytics.jsx
@@ -4,55 +4,64 @@ import { Button, Expand, CustomerItem } from "@components";
 import { BUTTON_VARIANTS } from "@constants/buttons";
 import { plusIcon } from "@assets/images";
 import { EXPAND_TITLES } from "@data";
+import { getAllCustomerService } from "../../services/customerService";
+import { v4 as uuidv4 } from "uuid";
 
 const Analytics = () => {
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
-    // Sử dụng fetch để fetch dữ liệu từ API
-    fetch("https://64e70a09b0fd9648b78f3e0a.mockapi.io/customers")
-      .then((response) => response.json())
-      .then((data) => {
-        // Lưu trữ dữ liệu từ API vào state
-        setCustomers(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    const getAllCustomers = async () => {
+      // Call getAllCustomerService function to get all data
+      const data = await getAllCustomerService();
+      setCustomers(data);
+    };
+    getAllCustomers();
   }, []);
+
+  const renderList = () => {
+    return (
+      <ul className="customer__list">
+        {customers.map((customer) => (
+          <li className="customer__item" key={uuidv4()}>
+            <CustomerItem
+              avatar={customer.avatar}
+              name={customer.name}
+              email={customer.mail}
+              phoneNumber={customer.phoneNumber}
+              gender={customer.gender}
+            />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <>
+    <div className="analytics">
       <div className="analytics__header">
         <h1 className="analytics__title">Customer List</h1>
         <Button variant={BUTTON_VARIANTS.SECONDARY} icon={plusIcon} />
       </div>
-
-      <div className="customer__table">
-        {/* Start sort title */}
-        <div className="customer__sort">
-          {EXPAND_TITLES.map((EXPAND_TITLE) => (
-            <div className="sort__item" key={EXPAND_TITLE.id}>
-              <Expand expandName={EXPAND_TITLE.title} />
-            </div>
-          ))}
+      {customers.length > 0 ? (
+        <div className="customer__table">
+          {/* Start sort title */}
+          <div className="customer__sort">
+            {EXPAND_TITLES.map((EXPAND_TITLE) => (
+              <div className="sort__item" key={EXPAND_TITLE.id}>
+                <Expand expandName={EXPAND_TITLE.title} />
+              </div>
+            ))}
+          </div>
+          {renderList()}
         </div>
-
-        {/* Start customer list */}
-        <ul className="customer__list">
-          {customers.map((customer) => (
-            <li className="customer__item" key={customer.id}>
-              <CustomerItem
-                avatar={customer.avatar}
-                name={customer.name}
-                email={customer.mail}
-                phoneNumber={customer.phoneNumber}
-                gender={customer.gender}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+      ) : (
+        // Show message when list is empty
+        <div className="empty__list">
+          <p className="empty__message">Customer list is empty!</p>
+        </div>
+      )}
+    </div>
   );
 };
 
--- practice-one/src/router/Routers.jsx
+++ practice-one/src/router/Routers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Routers.css"
+import "./Routers.css";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import { Sidebar } from "@layouts";
 import { Home, Analytics } from "@pages";
@@ -7,13 +7,15 @@ import { Home, Analytics } from "@pages";
 const Routers = () => {
   return (
     <Router>
-      <Sidebar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/analytics" element={<Analytics />} />
-        </Routes>
-      </main>
+      <div className="contents">
+        <Sidebar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/analytics" element={<Analytics />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 };
