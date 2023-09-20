db4d5c81ec3343cd678b61252442d39eedf538a5
datdragon777 <ducdat612001@gmail.com>
2023-09-20 09:40:06 +0700

    Update contact storybook

--- practice-one/src/components/Contact/Contact.stories.jsx
+++ practice-one/src/components/Contact/Contact.stories.jsx
@@ -4,10 +4,22 @@ export default {
   title: "Components/Contact",
   component: Contact,
   tags: ["autodocs"],
-  argTypes: {},
+  argTypes: {
+    selectedCustomer: {
+      table: {
+        disable: false,
+      }
+    }
+  },
 };
 
 const Template = (args) => <Contact {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  selectedCustomer: {
+    mail: "decker_ultra1780@ultraman.com",
+    phoneNumber: "0457863245",
+    address: "123 Main St, City, Country",
+  },
+};
