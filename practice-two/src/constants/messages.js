7a622e38873e9c9aaef7c93795306f9483203690
datdragon777 <ducdat612001@gmail.com>
2023-10-09 15:41:24 +0700

    Update group messages

--- practice-two/src/constants/messages.js
+++ practice-two/src/constants/messages.js
@@ -1,18 +1,14 @@
-const ERROR_MESSAGE = {
-  ERROR_GET_API: "Having some issues on server",
-  ADD_FAILED: "Create new customer failed",
-  UPDATE_FAILED: "Update customer failed",
-  DELETE_FAILED: "Delete customer failed",
-};
-
-const SUCCESS_MESSAGE = {
-  ADD_SUCCESSED: "Create new customer successfully",
-  UPDATE_SUCCESSED: "Update customer successfully",
-  DELETE_SUCCESSED: "Delete customer successfully",
-};
-
-const EMPTY_MESSAGE = {
+export const MESSAGES = {
+  ERRORS: {
+    GET_API: "Having some issues on server",
+    ADD_FAILED: "Create new customer failed",
+    UPDATE_FAILED: "Update customer failed",
+    DELETE_FAILED: "Delete customer failed",
+  },
+  SUCCESSES: {
+    ADD_SUCCESSED: "Create new customer successfully",
+    UPDATE_SUCCESSED: "Update customer successfully",
+    DELETE_SUCCESSED: "Delete customer successfully",
+  },
   EMPTY_LIST: "Customer list is empty!",
 };
-
-export { ERROR_MESSAGE, SUCCESS_MESSAGE, EMPTY_MESSAGE };
