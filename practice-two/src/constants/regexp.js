59d9b360429b33256a6baf69bd232aa6c6f3dc4f
datdragon777 <ducdat612001@gmail.com>
2023-10-11 10:23:15 +0700

    Update new regex for avatar, email and phone number

--- practice-two/src/constants/regexp.js
+++ practice-two/src/constants/regexp.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-useless-escape */
 export const REGEX = {
   USERNAME: '^[A-Za-z0-9]{3,16}$',
-  AVATAR: '\.(jpg|jpeg|png|gif|bmp|tiff|webp|svg|ico)$',
-  EMAIL: '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-  PHONENUMBER: '^(((\+|)84)|0)([1-9]{1})([0-9]{8})\b',
+  AVATAR: '.*.(jpg|jpeg|png|gif|bmp|tiff|webp|svg|ico)$',
+  EMAIL: '^[w-.]+@([w-]+.)+[w-]{2,4}$',
+  PHONENUMBER: '\\d{10}',
   DESCRIPTION: '^.{3,20}$',
   ADDRESS: '.{8,}',
 };
- 
