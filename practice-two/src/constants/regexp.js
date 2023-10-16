d1b299edf350553f49e392ee1c4a57d80aef2eb3
datdragon777 <ducdat612001@gmail.com>
2023-10-16 09:02:46 +0700

    Update regex for username

--- practice-two/src/constants/regexp.js
+++ practice-two/src/constants/regexp.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-escape */
 export const REGEX = {
-  USERNAME: '^[A-Za-z0-9]{3,16}$',
+  USERNAME: '.{3,16}',
   AVATAR: '.*.(jpg|jpeg|png|gif|bmp|tiff|webp|svg|ico)$',
   EMAIL: '^[w-.]+@([w-]+.)+[w-]{2,4}$',
   PHONENUMBER: '\\d{10}',
