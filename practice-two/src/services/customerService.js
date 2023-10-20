77ce63dfde3311657d9a6d5173b647af5bf1f4df
datdragon777 <ducdat612001@gmail.com>
2023-10-20 17:59:50 +0700

    Update create/update/delete feature

--- practice-two/src/services/customerService.js
+++ practice-two/src/services/customerService.js
@@ -1,12 +1,51 @@
-// import { BASE_URL, PATH } from "@constants";
+import { BASE_URL, PATH } from '@constants';
 
 const getAllCustomerService = async (url) => {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error("An error occurred while fetching the data.");
+    throw new Error('An error occurred while fetching the data.');
   }
   const data = await response.json();
   return data;
 };
 
-export default getAllCustomerService;
+const request = async (path, method, data) => {
+  const url = `${BASE_URL}/${path}`;
+  const response = await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Error while sending request');
+  }
+};
+
+const createCustomerService = (customerData) => {
+  return request(`${PATH}`, 'POST', customerData);
+};
+
+const getCustomerByIdService = (id) => {
+  return request(`${PATH}/${id}`, 'GET');
+};
+
+const updateCustomerService = (id, customerData) => {
+  return request(`${PATH}/${id}`, 'PUT', customerData);
+};
+
+const deleteCustomerService = (id) => {
+  return request(`${PATH}/${id}`, 'DELETE');
+};
+
+export {
+  getAllCustomerService,
+  createCustomerService,
+  getCustomerByIdService,
+  updateCustomerService,
+  deleteCustomerService,
+};
